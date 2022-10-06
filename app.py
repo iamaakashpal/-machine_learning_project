019472f498ebc5c2e9c40c50f42d59c914fc402b
iamaakashpal <aakashpal1198@gmail.com>
2022-10-06 13:12:18 +0530

    File Updated

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/",methods=['GET','POST'])
 def index():
-    return "Starting Machine Learning Project"
+    return "Started"
 
 if __name__ == '__main__':
     app.run(debug=True)
