495d59d61e98164820e4692513ce22966a9c683d
iamaakashpal <aakashpal1198@gmail.com>
2022-10-06 13:39:39 +0530

    File Added

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from housing.logger import logging
 app = Flask(__name__)
 
 @app.route("/",methods=['GET','POST'])
 def index():
-    return "Started"
+    logging.info("We are testing logged module")
+    return "CI CD Pipeline"
 
 if __name__ == '__main__':
     app.run(debug=True)
