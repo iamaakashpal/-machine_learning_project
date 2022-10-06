5bdbb7a47acb6f1752cb34e7b31f76775940bc79
iamaakashpal <aakashpal1198@gmail.com>
2022-10-06 17:58:28 +0530

    Exception File Updated

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
 from flask import Flask
+import sys
 from housing.logger import logging
+from housing.exception import HousingException
 app = Flask(__name__)
+
 
 @app.route("/",methods=['GET','POST'])
 def index():
-    logging.info("We are testing logged module")
+    try:
+        raise Exception("We are testing custom Exception")
+    except Exception as e:
+        housing = HousingException(e,sys)
+        logging.info(housing.error_message)
+        logging.info("We are testing logging module")
     return "CI CD Pipeline"
+
 
 if __name__ == '__main__':
     app.run(debug=True)
