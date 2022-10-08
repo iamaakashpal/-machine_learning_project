966002c9430bde247fa578a5e10d2fb2bd4766eb
iamaakashpal <aakashpal1198@gmail.com>
2022-10-08 22:05:17 +0530

    File Updated

--- housing/config/configuration.py
+++ housing/config/configuration.py
@@ -8,12 +8,36 @@ from housing.constant import *
 
 class Configuartion:
     def __init__(self,config_file_path:str = CONFIG_FILE_PATH,current_time_stamp:str = CURRENT_TIME_STAMP) -> None:
-        self.config_info = read_yaml_file(file_path = config_file_path)
-        self.training_pipeline_config = self.get_training_pipeline_config()
-        self.time_stamp = current_time_stamp
+        try:
+            self.config_info = read_yaml_file(file_path = config_file_path)
+            self.training_pipeline_config = self.get_training_pipeline_config()
+            self.time_stamp = current_time_stamp
+        except Exception as e:
+            raise HousingException(e,sys) from e
 
     def get_data_ingestion_config(self) -> DataIngestionConfig:
-        pass
+        try:
+            data_ingestion_info = self.config_info[DATA_INGESTION_CONFIG_KEY]
+            
+            dataset_download_url = data_ingestion_info[DATA_INGESTION_DOWNLOAD_URL_KEY]
+            tgz_download_dir = os.path.join(
+                
+            )
+            raw_data_dir = data_ingestion_info[]
+            ingested_train_dir = data_ingestion_info[]
+            ingested_test_dir = data_ingestion_info[]
+            
+            data_ingestion_config = DataIngestionConfig(
+                dataset_download_url = dataset_download_url,
+                tgz_download_dir = tgz_download_dir,
+                raw_data_dir = raw_data_dir,
+                ingested_train_dir = ingested_train_dir,
+                ingested_test_dir = ingested_test_dir
+            )
+            logging.info(f"Data Ingestion config:{data_ingestion_config}")
+            return data_ingestion_config
+        except Exception as e:
+            raise HousingException(e,sys) from e        
 
     def get_data_validation_config(self) -> DataValidationConfig:
         pass
